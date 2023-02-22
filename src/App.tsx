159c5a111794f4650229404531dfd9edf214bdae
ElijahBond <bondarefil@gmail.com>
2023-02-22 22:35:26 +0300

    add TodoItem and NewTodoForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import NewTodoForm from './components/NewTodoForm';
+import TodoItem from './components/Todoitem';
 
 function App() {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value)
+  }
+
+  const addTodo = () => {
+    setTodos([text, ...todos]);
+    setText('')
+  }
+
   return (
     <div className="App">
-      
+      <NewTodoForm
+        value={text}
+        onChange={handleInput}
+        handleClick={addTodo}
+      />
+      <TodoItem 
+        id='111' 
+        completed={false} 
+        title='first todo' 
+        style={{border: '1px solid yellow'}} />
     </div>
   );
 }
