4a67ee05beb7dab04f6fdfd92e0aa2b0c0364f41
ElijahBond <bondarefil@gmail.com>
2023-02-23 12:06:00 +0300

    add types folder and update tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Todo } from './types';
+
 import './App.css';
+
 import NewTodoForm from './components/NewTodoForm';
 import TodoItem from './components/Todoitem';
 
 function App() {
   const [text, setText] = useState('');
-  const [todos, setTodos] = useState<string[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value)
   }
 
   const addTodo = () => {
-    setTodos([text, ...todos]);
+    const newTodo: Todo = {
+      id: new Date().toString(),
+      title: text,
+      completed: false
+    }
+    setTodos([newTodo, ...todos]);
     setText('')
   }
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then((data: Todo[]) => {
+        setTodos(data);
+      })
+  }, [])
 
   return (
     <div className="App">
